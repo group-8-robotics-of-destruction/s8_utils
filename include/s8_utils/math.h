4f366ca1a33790f5badaa0fad316113214e8e1e0
Lucas Wiener <lucas@wiener.se>
2014-11-08 21:03:54 +0100

    Created namespace math. Added sign function

--- include/s8_utils/math.h
+++ include/s8_utils/math.h
@@ -5,13 +5,22 @@
 
 namespace s8 {
 	namespace utils {
+		namespace math {
+			/**
+			 * Returns true if the given value is relatively close to zero. Otherwise false.
+			 * The relative treshold can be set as second parameter.
+			 */
+			bool is_zero(double value, double treshold = 0.000001) {
+				return std::abs(value) < 0.000001;
+			}
 
-		/**
-		 * Returns true if the given value is relatively close to zero. Otherwise false.
-		 * The relative treshold can be set as second parameter.
-		 */
-		bool is_zero(double value, double treshold = 0.000001) {
-			return std::abs(value) < 0.000001;
+			/**
+			 * Returns the sign of the given value. Will return -1 for negative values. Will return 1 for positive values. Will return 0 for 0.
+			 */
+			template <typename T>
+			int sign(T val) {
+			    return (T(0) < val) - (val < T(0));
+			}
 		}
 	}
 }
